82498d245315ceac517693b0f2483d8b58bfdc2c
Sam Ford <1584702+samford@users.noreply.github.com>
2025-01-15 10:56:01 -0500

    cmd/aspell-dictionaries: raise on error conditions
    The `aspell-dictionaries` command doesn't check for errors, so
    failures may not be clear. This adds `raise` calls to make it clear
    when and why the command isn't working as expected.
    
    Namely, this adds `--fail` to the `curl_output` arguments, so the
    command will have a failing exit status if it receives an HTTP error
    (e.g., a 404 Not Found response). This allows us to use
    `result.status.success?` to ensure that we received a good response.
    
    This also raises if we fail to identify languages from the HTML
    content. Without this, the command completes without producing any
    output and it's not clear what went wrong.

--- cmd/aspell-dictionaries.rb
+++ cmd/aspell-dictionaries.rb
@@ -25,14 +25,18 @@ module Homebrew
       def run
         dictionary_url = "https://ftp.gnu.org/gnu/aspell/dict"
         dictionary_mirror = "https://ftpmirror.gnu.org/aspell/dict"
+        index_url = "#{dictionary_url}/0index.html"
         languages = {}
 
-        index_output = Utils::Curl.curl_output("#{dictionary_url}/0index.html").stdout
-        index_output.scan(FILE_PATH_REGEX).each do |match|
+        result = Utils::Curl.curl_output("--fail", index_url)
+        raise "Unable to fetch language index page: #{index_url}" unless result.success?
+
+        result.stdout.scan(FILE_PATH_REGEX).each do |match|
           # The first capture group is the file path (e.g. `en/aspell6-en-2020.12.07-0.tar.bz2`)
           # The second capture group is the language (e.g. `en`)
           languages[T.must(match[1]).tr("-", "_")] = match[0]
         end
+        raise "Unable to identify languages at #{index_url}" if languages.blank?
 
         resources = languages.map do |language, path|
           r = Resource.new(language)
