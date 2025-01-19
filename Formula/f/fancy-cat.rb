c89ff8bee7013fc85c82645548e6f503d3cf7bc6
Rui Chen <rui@chenrui.dev>
2025-01-18 20:27:34 -0500

    fancy-cat 0.1.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/f/fancy-cat.rb
@@ -0,0 +1,32 @@
+class FancyCat < Formula
+  desc "PDF reader for terminal emulators using the Kitty image protocol"
+  homepage "https://github.com/freref/fancy-cat"
+  url "https://github.com/freref/fancy-cat/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "8d07869f94eb6105a52684e13f67f3d844ea1bc54183dfeee20eb4f8f51524ce"
+  license "MIT"
+
+  depends_on "zig" => :build
+  depends_on "mupdf"
+
+  def install
+    # Fix illegal instruction errors when using bottles on older CPUs.
+    # https://github.com/Homebrew/homebrew-core/issues/92282
+    cpu = case Hardware.oldest_cpu
+    when :arm_vortex_tempest then "apple_m1" # See `zig targets`.
+    else Hardware.oldest_cpu
+    end
+
+    args = %W[
+      --prefix #{prefix}
+      -Doptimize=ReleaseSafe
+    ]
+
+    args << "-Dcpu=#{cpu}" if build.bottle?
+    system "zig", "build", *args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/fancy-cat --version")
+    assert_match "error: InvalidArguments", shell_output("#{bin}/fancy-cat 2>&1", 1)
+  end
+end
